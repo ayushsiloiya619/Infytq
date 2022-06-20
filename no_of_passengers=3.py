bb91d44c9b6cdb8abab70ce9aed3b0fb9a7e3b06
AYUSH SILOIYA <85118674+ayushsiloiya619@users.noreply.github.com>
2022-06-21 01:05:14 +0530

    Delete no_of_passengers=3.py

--- no_of_passengers=3.py
+++
@@ -1,7 +0,0 @@
-no_of_passengers=3
-ticket_number=1001
-print("Ticket Numbers for all the Passengers:")
-while(no_of_passengers>0):
-    print("T -",ticket_number)
-    ticket_number=ticket_number+1
-    no_of_passengers=no_of_passengers-1
